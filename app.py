8d136fadfc60f2d696e0727a38fb5ad6c7dc6784
gazaboo <gazaboo@pop-os.localdomain>
2023-12-03 16:55:33 +0100

    improve layout

--- app.py
+++ app.py
@@ -9,30 +9,33 @@ def display_module_infos(parcours, semestre, module):
     data = df.query(
         'Parcours == @parcours and Semestre == @semestre and Module == @module')
     data = data.iloc[0]
-    for col in data.index:
-        st.subheader(col)
+
+    title = ''.join(data.Module.split(':')[1:])
+    st.title(title)
+    st.header(' - '.join([data.Semestre, data.Parcours]))
+    cols = ['Competence Ciblee', 'Descriptif',
+            'Apprentissages Critiques', 'Mots Cles']
+    for col in cols:
+        st.subheader(f':blue[{col}]')
         st.write(data[col])
-
-
-def set_stage():
-    st.write('test')
 
 
 def display_modules_main_panel(parcours):
     st.title(parcours)
     for semestre in data.Semestre.unique():
-        st.subheader(semestre)
-        parcours_semestre = data.query('Semestre == @semestre')
-        for module in parcours_semestre.Module:
-            label = ' '.join(module.split(':')[1:])
-            st.button(
-                label=label,
-                key=semestre + label,
-                on_click=display_module_infos,
-                args=(parcours, semestre, module)
-            )
+        with st.expander(semestre):
+            parcours_semestre = data.query('Semestre == @semestre')
+            for module in parcours_semestre.Module:
+                label = ' '.join(module.split(':')[1:])
+                st.button(
+                    label=label,
+                    key=semestre + label,
+                    on_click=display_module_infos,
+                    args=(parcours, semestre, module)
+                )
 
 
+st.sidebar.title('Parcours')
 for parcours in df.Parcours.unique():
     if st.sidebar.button(parcours, type="primary"):
         data = df.query('Parcours == @parcours')
